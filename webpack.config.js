5006337ba8e55575fc31a29c86bd58e17f264144
ioanachi <ioana_chichi@outlook.com>
2018-01-02 16:39:12 +0200

    works

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './app.js',
+  entry: {
+    app: ['./app.js']
+  },
+  watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  watch:true,
   plugins: [
-
     new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
       host: 'localhost',
       port: 3000,
       server: {
@@ -28,22 +27,35 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.html$/,
+        test: /\.js$/,
+        exclude: [/node_modules/],
         use: [{
-          loader: 'html-loader',
+          loader: 'babel-loader',
           options: {
-            minimize:true
-          }
-        }]
+            presets: ['es2015']
+          },
+        }],
       },
-
-      // Loaders for other file types can go here
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
     ],
   },
-
-
-
-
-
-
-}
+};
